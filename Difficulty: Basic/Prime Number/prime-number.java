e64f269dcac15cc5291d8cb94ce12d5fa4a918d2
Khushi-b08 <141702583+Khushi-b08@users.noreply.github.com>
2025-05-21 12:31:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Prime Number/prime-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
