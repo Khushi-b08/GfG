e731c45fea2d03be1056d70f5bfcbd0e14291962
Khushi-b08 <141702583+Khushi-b08@users.noreply.github.com>
2025-05-22 09:38:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Valid Triangle/valid-triangle.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+class Solution {
+    public boolean checkValidity(int a, int b, int c) {
+        // code here
+        if((a+b)>c&&(b+c)>a&&(c+a)>b){
+            return true;
+        }
+        return false;
+    }
+}
