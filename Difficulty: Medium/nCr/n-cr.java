b8deb33f79499d6a64e0ff2b4f1b1fc734c23661
Khushi-b08 <141702583+Khushi-b08@users.noreply.github.com>
2025-05-24 12:49:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int nCr(int n, int r) {
+        // code here
+        if (r > n) return 0;
+        if (r > n - r) r = n - r; // because C(n, r) == C(n, n - r)
+
+        long res = 1;
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+
+        return (int) res;
+    }
+}
